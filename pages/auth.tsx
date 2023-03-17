e973a1f66847ffa98d8fef10a1e9166537b9b772
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-17 14:21:12 +0530

    prisma init and atlas env setup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,11 +1,18 @@
 import Input from "@/components/Input";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -14,17 +21,22 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>f
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
             <div className="flex flex-col gap-4">
-              <Input
-                label="Username"
-                onChange={(e: {
-                  target: { value: React.SetStateAction<string> };
-                }) => setUserName(e.target.value)}
-                id="username"
-                type="text"
-                value={userName}
-              />
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e: {
+                    target: { value: React.SetStateAction<string> };
+                  }) => setUserName(e.target.value)}
+                  id="username"
+                  type="text"
+                  value={userName}
+                />
+              )}
+
               <Input
                 label="Email"
                 onChange={(e: {
@@ -48,12 +60,17 @@ const Auth = () => {
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
               type="submit"
             >
-              Login
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
-              First time using Netflix ?{" "}
-              <span className="text-white ml-1 cursor-pointer hover:underline">
-                Create an account
+              {variant === "login"
+                ? "First time using Netflix ?"
+                : "Already have an account?"}{" "}
+              <span
+                className="text-white ml-1 cursor-pointer hover:underline"
+                onClick={toggleVariant}
+              >
+                {variant === "login" ? "Create an account" : "Login"}{" "}
               </span>
             </p>
           </div>
