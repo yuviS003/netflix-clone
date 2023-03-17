d5f89fc707714ff05823ef067f8dc7607b8db920
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-17 16:09:11 +0530

    google and github auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
-// import { PrismaAdapter } from "@next-auth/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { compare } from "bcrypt";
 import prismadb from "@/lib/prismadb";
 
@@ -61,7 +61,7 @@ export default NextAuth({
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
-  // adapter: PrismaAdapter(prismadb),
+  adapter: PrismaAdapter(prismadb),
   session: { strategy: "jwt" },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,23 +1,47 @@
-import Input from "@/components/Input";
 import axios from "axios";
-import Image from "next/image";
-import React, { useCallback, useState } from "react";
-import { signIn } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import Input from "@/components/Input";
+
+// export async function getServerSideProps(context: NextPageContext) {
+//   const session = await getSession(context);
+//   console.log(session);
+
+//   if (session) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {},
+//   };
+// }
 
 const Auth = () => {
   const router = useRouter();
+
   const [email, setEmail] = useState("");
-  const [userName, setUserName] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+
   const [variant, setVariant] = useState("login");
+
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
 
-  const Login = useCallback(async () => {
+  const login = useCallback(async () => {
     try {
       await signIn("credentials", {
         email,
@@ -25,30 +49,32 @@ const Auth = () => {
         redirect: false,
         callbackUrl: "/",
       });
-      router.push("/");
-    } catch (err) {
-      console.error(err);
+
+      router.push("/profiles");
+    } catch (error) {
+      console.log(error);
     }
   }, [email, password, router]);
 
-  const Register = useCallback(async () => {
+  const register = useCallback(async () => {
     try {
-      await axios.post(`/api/register`, {
+      await axios.post("/api/register", {
         email,
-        userName,
+        name,
         password,
       });
-      Login();
-    } catch (err) {
-      console.error(err);
+
+      login();
+    } catch (error) {
+      console.log(error);
     }
-  }, [email, userName, password, Login]);
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <Image src="/images/logo.png" alt="logo" width="120" height="0" />
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
@@ -58,52 +84,59 @@ const Auth = () => {
             <div className="flex flex-col gap-4">
               {variant === "register" && (
                 <Input
-                  label="Username"
-                  onChange={(e: {
-                    target: { value: React.SetStateAction<string> };
-                  }) => setUserName(e.target.value)}
-                  id="username"
+                  id="name"
                   type="text"
-                  value={userName}
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
-
               <Input
-                label="Email"
-                onChange={(e: {
-                  target: { value: React.SetStateAction<string> };
-                }) => setEmail(e.target.value)}
                 id="email"
                 type="email"
+                label="Email address or phone number"
                 value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                label="Password"
-                onChange={(e: {
-                  target: { value: React.SetStateAction<string> };
-                }) => setPassword(e.target.value)}
-                id="password"
                 type="password"
+                id="password"
+                label="Password"
                 value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
             <button
-              onClick={variant === "login" ? Login : Register}
+              onClick={variant === "login" ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
-              type="submit"
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={32} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={32} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
-                ? "First time using Netflix ?"
-                : "Already have an account?"}{" "}
+                ? "First time using Netflix?"
+                : "Already have an account?"}
               <span
-                className="text-white ml-1 cursor-pointer hover:underline"
                 onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
               >
-                {variant === "login" ? "Create an account" : "Login"}{" "}
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
+              .
             </p>
           </div>
         </div>
