89ed55a3f84ab484292240836aec68b8017fff5b
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-18 17:10:48 +0530

    billboards

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,7 +1,38 @@
+import useBillboard from "@/hooks/useBillboard";
 import React from "react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const Billboard = () => {
-  return <div>Billboard</div>;
+  const { data } = useBillboard();
+  return (
+    <div className="relative h-[68vh]">
+      <video
+        className="w-full h-full object-cover brightness-[60%]"
+        autoPlay
+        muted
+        loop
+        poster={data?.thumbnailUrl}
+        src={data?.videoUrl}
+      ></video>
+      <div className="absolute top-[30%] md:top-[36%] ml-4 md:ml-16">
+        <p className="text-white text-xl md:text-5xl h-full w-1/2 lg:text-6xl font-bold drop-shadow-sm-xl">
+          {data?.title}
+        </p>
+        <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-1/2 drop-shadow-lg">
+          {data?.description}
+        </p>
+        <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <button
+            type="button"
+            className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-2 w-auto text-xs lg:text-lg font-semibold flex items-center hover:bg-opacity-20 transition gap-2"
+          >
+            <AiOutlineInfoCircle />
+            More Info
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Billboard;
